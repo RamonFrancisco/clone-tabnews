62182644cc368c4301e4f31bd5844cc5b13e9054
Ramon <RamonFrancisco@users.noreply.github.com>
2024-03-11 01:56:47 +0000

    Add `database`and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  return response.status(200).json({ chave: "curso topzera" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const query = await database.query("SELECT 1 + 1 as sum");
+  console.log(query.rows);
+  return response.status(200).json(query);
 }
 
 export default status;
