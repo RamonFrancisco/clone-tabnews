922ad1b3cc07f4dfa134539f53ca8cfcc7383be2
Ramon <RamonFrancisco@users.noreply.github.com>
2024-03-12 03:50:44 +0000

    More `database.js` robust with error handling

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,17 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
 
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-  query: query,
+  query,
 };
