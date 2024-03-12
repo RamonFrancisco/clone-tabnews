49c8506fb2a2ebcd9b9f23b428fab32326eaf57c
Ramon <RamonFrancisco@users.noreply.github.com>
2024-03-12 03:52:34 +0000

    Finish endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const query = await database.query("SELECT 1 + 1 as sum");
-  console.log(query.rows);
-  return response.status(200).json(query);
+  const updatedAt = new Date().toISOString();
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections");
+  const dbMaxConnectionsValue = dbMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const dbOpenedConnectionsValue = dbOpenedConnections.rows[0].count;
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,11 @@
 test("GET to '/api/v1/status' should return status 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+  const responseJson = await response.json();
+
+  const parsedUpdatedAt = new Date(responseJson.updated_at).toISOString();
+  expect(responseJson.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseJson.dependencies.database.version).toEqual("16.0");
+  expect(responseJson.dependencies.database.max_connections).toEqual(100);
+  expect(responseJson.dependencies.database.opened_connections).toEqual(1);
 });
